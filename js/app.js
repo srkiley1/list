6307c61cd9fac63923a1373093c8d74bb10df2ce
srkiley1 <srk2@att.net>
2015-07-20 15:42:45 -0700

    index.html formatted and chanes to css/main.css and js/app.js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+$(document).ready( function(){
+	  
+// $("#items").prepend("<li>pita bread</li><li>oil</li><li>lemon</li>");
+
+
+$("#text").click(function(){ 
+//	alert($("#text2").val());
+$("#items").prepend("<li class='list-item'>" + $("#text2").val() + "</li>");
+
+	 });
+$('button').click(function() {
+	$('.list-item').first().remove();
+	});
+
+});
