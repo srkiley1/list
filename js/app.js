9d25d584132f9a2633e56283795e97741c16eb55
srkiley1 <srk2@att.net>
2015-07-22 16:44:00 -0700

    better?

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 
 $(document).ready( function(){
 	  
-// $("#items").prepend("<li>pita bread</li><li>oil</li><li>lemon</li>");
+	// $("#items").prepend("<li>pita bread</li><li>oil</li><li>lemon</li>");
 
 
-$("#text").click(function(){ 
-//	alert($("#text2").val());
-$("#items").prepend("<li class='list-item'>" + $("#text2").val() + "</li>");
+	$("#text").click(function(){ 
+	//	alert($("#text2").val());
+	$("#items").prepend("<li class='list-item'>" + $("#text2").val() + "</li>");
 
-	 });
-$('button').click(function() {
-	$('.list-item').first().remove();
+		 });
+	$('button').click(function() {
+		$('.list-item').first().remove();
+		});
 	});
-
-});
