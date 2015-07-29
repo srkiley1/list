52950721e32c1d494e1acc405fec9592bd518372
srkiley1 <srk2@att.net>
2015-07-28 18:16:34 -0700

    items on left

--- js/app.js
+++ js/app.js
@@ -2,14 +2,12 @@
 $(document).ready( function(){
 	  
 	// $("#items").prepend("<li>pita bread</li><li>oil</li><li>lemon</li>");
-
-
-	$("#text").click(function(){ 
+	$("#text").click(function(){
 	//	alert($("#text2").val());
-	$("#items").prepend("<li class='list-item'>" + $("#text2").val() + "</li>");
-
+	$(".col-md-2").prepend("<li class='list-item'>" + $("#text2").val() + "</li>");
 			});
 	$('button').click(function() {
-		$('.list-item').first().remove();
+		$('.list-item').last().remove();
 			});
+
 });
